57b8de13929109baad237b3663870c95aef4cfb2
Zeanon <thezeanon@gmail.com>
2025-05-13 17:02:09 +0200

    .

--- publish.bat
+++ publish.bat
@@ -2,7 +2,7 @@
 
 @REM publish the plugin
 dotnet publish^
-    --framework net8.0^
+    --framework net8.0-windows^
     --runtime win-x64^
     --self-contained false^
     --configuration Release^
